a96063c66ad6e758345f06d97756dc8dc77583eb
Bohdan Filimonyuk <bogdan1999kovel@gmail.com>
2022-03-26 03:30:14 +0100

    Validation and error handling

--- client-app/src/app/layout/navbar.tsx
+++ client-app/src/app/layout/navbar.tsx
@@ -11,6 +11,7 @@ export default function NavBar() {
                     Reactivities
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='activities' />
+                <Menu.Item as={NavLink} to='/errors' name='errors' />
                 <Menu.Item as={NavLink} to='/createactivity'>
                     <Button as={Link} to='/createactivity' positive content='Create Activity' />
                 </Menu.Item>
